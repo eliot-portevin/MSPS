eb3346ee3749670884270b49569eb78293443030
eliot-portevin <eliotportevin@gmail.com>
2024-01-12 16:13:53 +0100

    Put log files in log directory

--- logger.py
+++ logger.py
@@ -1,21 +1,30 @@
 from datetime import datetime
-from os.path import exists
-
+import os
 
 class Logger:
+    LOGS_DIRECTORY = 'logs'
+
     def __init__(self):
         self.date = datetime.now().strftime('%d-%m-%Y')
-        self.log_file = f'{self.date}.log'
+        self.log_file = os.path.join(self.LOGS_DIRECTORY, f'{self.date}.log')
         self.initiate_log_file()
 
-    def log(self, message):
-        date = datetime.now().strftime('%H:%M:%S')
-        message = f'[{date}] - {message}'
+    @classmethod
+    def current_timestamp(cls):
+        return datetime.now().strftime('%H:%M:%S')
 
-        with open(self.log_file, 'a') as f:
-            f.write(message + '\n')
+    def log(self, message):
+        timestamp = self.current_timestamp()
+        log_entry = f'[{timestamp}] - {message}'
+
+        try:
+            with open(self.log_file, 'a') as f:
+                f.write(log_entry + '\n')
+        except Exception as e:
+            print(f'Error writing to log file: {e}')
 
     def initiate_log_file(self):
-        if not exists(self.log_file):
-            with open(self.log_file, 'w') as f:
-                f.write('')
+        try:
+            os.makedirs(self.LOGS_DIRECTORY, exist_ok=True)
+        except Exception as e:
+            print(f'Error creating log directory: {e}')
